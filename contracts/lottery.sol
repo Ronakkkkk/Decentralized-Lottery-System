ac8c139a81a0e2a989cd24c596ca113b90f1807d
Ronak <ronak01.raj@gmail.com>
2023-08-10 15:10:30 +0530

    final commit

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 
@@ -10,7 +10,8 @@ contract DecentralizedLottery {
     uint256 public minimumbet;
     uint256 public lotteryEnd;
     uint256 private seed;
-    constructor(uint256 _minimumbet,uint256 _durationInBlocks) {
+    event WinnerSelected(address winner);
+    constructor(uint256 _minimumbet,uint256 _durationInBlocks) payable {
         manager=msg.sender;
         minimumbet=_minimumbet;
         lotteryEnd=block.number+_durationInBlocks;
@@ -22,6 +23,7 @@ require(msg.value>=minimumbet, "Insufficinet Bet ammount");
 require(block.number<lotteryEnd,"Lottery has ended");
 
 players.push(msg.sender);
+console.log("%s entered", msg.sender);
         
     }
 
@@ -31,10 +33,21 @@ function pickWinner() public restricted {
     seed = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
     uint256 winnerIndex = seed % players.length;
     address winner = players[winnerIndex];
+    console.log("%s is the winner", winner);
 
     uint256 balance = address(this).balance;
     payable(winner).transfer(balance);
+    console.log("funds transferred");
 
     emit WinnerSelected(winner);
+}
+
+modifier restricted() {
+    require(msg.sender == manager, "Only the manager can perform this action");
+    _;
+}
+
+function setManager(address newManager) public restricted {
+    manager = newManager;
 }
 }
